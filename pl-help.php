701f58437227a28e8236f1142f742e9f8a64b0c6
umar83 <umarhatob@gmail.com>
2017-03-04 17:42:46 +0700

    Create pl-help.php

---
+++ pl-help.php
@@ -0,0 +1,60 @@
+<?php
+function pl_help()
+{
+	echo "<h1>Placing different link</h1>
+
+<ol><li>Adding Multiple Video Captions In JW Player 7
+	<ul>
+		<li>linkvideo@link_caption_1,CC1|link_caption_2,CC2|link_caption_3,CC3|link_caption_4,CC4|link_caption_n,CCn</li>
+	</ul>
+	</li>
+	<li>Google Drive
+	<ul>
+		<li>[yourjwplayer]https://drive.google.com/file/d/0Bzo0qld_8nElT1N6V3ZKa295RDQ/view@link_your_caption[/yourjwplayer]</li>
+	</ul>
+	</li>
+	
+	 <li>Google Photos
+	<ul>
+		<li>[yourjwplayer]https://photos.google.com/share/AF1QipOqcapC_WK69IoYSUK59cDup451Rne1IWKqw4OIuEykgEXVwqftYIjomCEkMZAGCQ/photo/AF1QipNF3pNvimKy9NsPegtw_DaZNdBRDmL1nzrAzLGM?key=OW9uUkZWUjBOb05YS1RaSWo3eGhTeTM2YUtqYnpB@link_your_caption[/yourjwplayer]</li>
+	</ul>
+	</li>
+    <li>Youtube
+	<ul>
+		<li>[yourjwplayer]https://www.youtube.com/watch?v=B4Myn1NpIXU@link_your_caption[/yourjwplayer]</li>
+	</ul>
+	</li>
+    <li>.MP4
+	<ul>
+		<li>[yourjwplayer]mp4post/http://yourjwplayer.online/vid/test.mp4@link_your_caption[/yourjwplayer]</li>
+	</ul>
+	</li>
+	<li>Amazon Cloud Drive
+	<ul>
+		<li>[yourjwplayer]amazon/PjnMa6OKcQHjR1pD1Bym87WMMA6YSL8W4vN1LoW1aAN@link_your_caption[/yourjwplayer]</li>
+	</ul>
+	</li>
+	<li>mp4upload.com
+	<ul>
+		<li>[yourjwplayer]http://www.mp4upload.com/f7rnz9yk0bgu@link_your_caption[/yourjwplayer]</li>
+	</ul>
+	</li>
+	<li>Facebook Video
+	<ul>
+		<li>[yourjwplayer]https://www.facebook.com/MagicNCS/videos/1134094403321872/@link_your_caption[/yourjwplayer]</li>
+	</ul>
+	</li>
+	<li>Vimeo.com
+	<ul>
+		<li>[yourjwplayer]https://vimeo.com/191583301/@link_your_caption[/yourjwplayer]</li>
+	</ul>
+	</li>
+	<li>onedrive.live.com
+	<ul>
+		<li>[yourjwplayer]https://onedrive.live.com/?cid=42d94c490d8798df&id=42D94C490D8798DF!120&ithint=video,mp4&authkey=!APOTZqYPLmz-Y68/@link_your_caption[/yourjwplayer]</li>
+	</ul>
+	</li>		
+</ol>
+";
+}
+?>
